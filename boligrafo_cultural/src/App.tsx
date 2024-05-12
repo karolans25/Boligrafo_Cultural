41fbe55e02d0951c469b3f441ecfa5454f0e955d
karolans25 <dev.carolina.pulido01@gmail.com>
2024-05-12 12:06:28 -0500

    Install tailwind con Vite + React
    Seguir los comandos de la página oficial de tailwind:
    
    https://tailwindcss.com/docs/guides/vite

--- boligrafo_cultural/src/App.tsx
+++ boligrafo_cultural/src/App.tsx
@@ -30,6 +30,11 @@ function App() {
       </p>
     </>
   )
+  // return (
+  //   <h1 className="text-3xl font-bold underline">
+  //     Hello world!
+  //   </h1>
+  // )
 }
 
 export default App
